a48abf5129eb6a30ae671bbf41a12972ef14de6e
Markus Ernst <markusrolandernst@t-online.de>
2022-02-03 09:30:48 +0000

    added code template

---
+++ codetemplate.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# _____________________________________________________________________________
+
+# ----------------
+# import libraries
+# ----------------
+
+# standard libraries
+# -----
+import torch
+import numpy
+
+# utilities
+# -----
+#from util import *
+
+# custom functions
+# -----
+def add_two_numbers(a,b):
+	"""
+	a: float, b:float -> float
+	add two numbers takes two float numbers a, b and
+	adds them together
+	"""
+	return a + b
+
+
+# custom classes
+# -----
+
+
+
+# ----------------
+# main program
+# ----------------
+
+if __name__ == "__main__":
+	print(add_two_numbers(a, b))
+	pass
+
+
+
+# _____________________________________________________________________________
+
+# Stick to 80 characters per line
+# Use PEP8 Style
+# Comment your code
+
+# -----------------
+# top-level comment
+# -----------------
+
+# medium level comment
+# -----
+
+# low level comment
