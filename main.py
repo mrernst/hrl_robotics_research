e9f0e515c5939e6a05b5e92fbeac935f41341342
Markus Ernst <markusrolandernst@t-online.de>
2022-02-10 15:02:11 +0000

    main training loop

--- main.py
+++ main.py
@@ -1,207 +1,258 @@
-# Python
-import numpy as np
-import os
-import random, datetime
+#!/usr/bin/python
+# _____________________________________________________________________________
+
+# ----------------
+# import libraries
+# ----------------
+
+# standard libraries
+# -----
 import argparse
+import os
+import sys
+import time
 
-# Plotting
-import matplotlib
-import matplotlib.pyplot as plt
-
-import imageio
-
-# Neural Networks
+import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
-import torchvision.transforms as T
-from torch.utils.tensorboard import SummaryWriter
+import gym
 
-# Gym environment
-import pybullet as p
-from pybullet_envs.bullet.kuka_diverse_object_gym_env import KukaDiverseObjectEnv
-from gym import spaces
+from util.launcher import get_default_params, run_experiment, \
+    add_launcher_base_args, save_args
+from util.replay_buffer import ReplayBuffer
+import algo.td3 as TD3
+from util.logger import MetricLogger
 
-# Custom utilities
-from util.agent import *
-from util.metrics import MetricLogger, TBMetricLogger
+from agent.agent import Agent
+# custom functions
+# -----
 
 
-# Flags
-parser = argparse.ArgumentParser()
-parser.add_argument(
-	 "-t",
-	 "--testrun",
-	 default=False,
-	 dest='testrun',
-	 action='store_true',
-	 help='reduced configuration for debugging')
-parser.add_argument(
-	 "-v",
-	 "--verbose",
-	 default=False,
-	 dest='verbose',
-	 action='store_true',
-	 help='print verbose error messages')
-parser.add_argument(
-	 "-e",
-	 "--evaluate",
-	 default=False,
-	 dest='evaluate',
-	 action='store_true',
-	 help='evaluate trained agent')
-parser.add_argument(
-	 "-a",
-	 "--algorithm",
-	 type=str,
-	 default='dqn',
-	 help="type of algorithm, choose 'dqn' or 'ppo'")
+def experiment(
+    policy_name="TD3",
+    env_name="InvertedPendulum-v2",
+    start_timesteps=25e3,
+    eval_freq=5e3,
+    max_timesteps=1e6,
+    expl_noise=0.1,
+    batch_size=256,
+    discount=0.99,
+    tau=0.005,
+    policy_noise=0.2,
+    noise_clip=0.5,
+    policy_freq=2,
+    save_model=False,
+    load_model="",
+    seed=0,  # This argument is mandatory
+    results_dir='./save'  # This argument is mandatory
+):
 
-args = parser.parse_args()
+    # Create results directory
+    results_dir = os.path.join(results_dir, str(seed))
+    os.makedirs(results_dir, exist_ok=True)
+    # Save arguments
+    save_args(results_dir, locals(), git_repo_path='./')
+    if save_model:
+        os.makedirs(os.path.join(results_dir, '/models'), exist_ok=True)
 
-# set global device variable, gpu vs cpu
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger = MetricLogger(results_dir)
+
+    file_name = f"{policy_name}_{env_name}_{seed}"
+    print("---------------------------------------")
+    print(f"Policy: {policy_name}, Env: {env_name}, Seed: {seed}")
+    print("---------------------------------------")
+
+    env = gym.make(env_name)
+    # Set seeds
+    env.seed(seed)
+    env.action_space.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    state_dim = env.observation_space.shape[0]
+    # state_dim = np.sum(
+    #    [env.observation_space.spaces[k].shape for k in env.observation_space.spaces.keys()])
+    action_dim = env.action_space.shape[0]
+    max_action = float(env.action_space.high[0])
+
+    # Initialize RL algorithm
+    kwargs = {
+        "state_dim": state_dim,
+        "action_dim": action_dim,
+        "max_action": max_action,
+        "discount": discount,
+        "tau": tau,
+    }
+
+    if policy_name == "TD3":
+        # Target policy smoothing is scaled wrt the action scale
+        kwargs["policy_noise"] = policy_noise * max_action
+        kwargs["noise_clip"] = noise_clip * max_action
+        kwargs["policy_freq"] = policy_freq
+        policy = TD3.TD3(**kwargs)
+    else:
+        raise NotImplementedError()
+
+    if load_model != "":
+        policy_file = file_name if load_model == "default" else load_model
+        policy.load(f"./models/{policy_file}")
+
+    # choose replay buffer
+    replay_buffer = ReplayBuffer(state_dim, action_dim)
+
+    # Initialize Agent with policy
+    agent = Agent(action_dim, policy, replay_buffer, burnin=start_timesteps)
+
+    # Evaluate untrained policy
+    agent.eval_policy(env_name, seed)
+    # delete: evaluations = [eval_policy(policy, env_name, seed)]
+
+    # Main training loop
+    # -----
+
+    # reset environment
+    state, done = env.reset(), False
+    # state = np.concatenate([state[k] for k in state.keys()])
+
+    # start logging of parameters
+    # initialize logger
+    # logger = utils.logger.TBMetricLogger()
+    episode_reward = 0
+    episode_timesteps = 0
+    episode_num = 0
+    success = 0
+
+    # training loop
+    for t in range(int(max_timesteps)):
+
+        episode_timesteps += 1
+
+        # 3. Render environment [WIP]
+        # env.render()
+
+        # 4. Run agent on the state (get the action)
+        # Select action randomly or according to policy
+
+        if t < start_timesteps:
+            action = env.action_space.sample()
+        else:
+            action = agent.select_action(state, max_action, expl_noise)
+
+        # 5. Agent performs action
+        next_state, reward, done, _ = env.step(action)
+        # next_state = np.concatenate([next_state[k] for k in next_state.keys()])
+
+        done_bool = float(
+            done) if episode_timesteps < env._max_episode_steps else 0
+
+        # 6. Remember
+        # Store data in replay buffer
+        agent.add_to_memory(state, action, next_state, reward, done_bool)
+
+        # 9. Update state
+        state = next_state
+        episode_reward += reward
+
+        # 8. Logging
+        logger.log_step(reward, None, None)
+        # 7. Learn
+        # Train agent after collecting sufficient data
+        if t >= start_timesteps:
+            agent.learn(batch_size, logger.writer)
+
+        if done:
+            if episode_timesteps < env._max_episode_steps:
+                success += 1
+                logger.writer.add_scalar(
+                    'agent/cumulative_success', success, t)
+            # +1 to account for 0 indexing. +0 on ep_timesteps
+            # since it will increment +1 even if done=True
+            print(" " * 80 + "\r" +
+                  f"Total T: {t+1} Episode Num: {episode_num+1} \
+                Episode T: {episode_timesteps} Reward: {episode_reward:.3f}",
+                  end="\r")
+
+            # Reset environment
+            state, done = env.reset(), False
+            # state = np.concatenate([state[k] for k in state.keys()])
+            episode_reward = 0
+            episode_timesteps = 0
+            episode_num += 1
+
+            # 10. Episode based metrics
+            # Evaluate episode
+            logger.log_episode(t)
+
+        if (t + 1) % eval_freq == 0:
+            agent.eval_policy(env_name, seed)
+            # log evaluation results with non-noisy action selection
+            logger.writer.add_scalar(
+                'agent/eval_episode_reward', agent.evaluations[-1], t)
+            agent.create_policy_eval_video(env_name, seed, results_dir + f"/t_{t+1}")
+            if save_model:
+                policy.save(f".{results_dir}/models/{file_name}")
 
 
-# helper functions to export movies
-def embed_mp4(filename):
-	import base64
-	import IPython
-	"""Embeds an mp4 file in the notebook."""
-	video = open(filename,'rb').read()
-	b64 = base64.b64encode(video)
-	tag = '''
-	<video width="640" height="480" controls>
-	<source src="data:video/mp4;base64,{0}" type="video/mp4">
-	Your browser does not support the video tag.
-	</video>'''.format(b64.decode())
-	
-	return IPython.display.HTML(tag)
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    # Place your experiment arguments here
+    arg_conf = parser.add_argument_group('Config')
+    # Policy name (TD3, or else if implemented)
+    arg_conf.add_argument("--policy_name", type=str)
+    # OpenAI gym environment name
+    arg_conf.add_argument("--env_name", type=str)
+    # Time steps initial random policy is used
+    arg_conf.add_argument("--start_timesteps", type=int)
+    # How often (time steps) we evaluate
+    arg_conf.add_argument("--eval_freq", type=int)
+    # Max time steps to run environment
+    arg_conf.add_argument("--max_timesteps", type=int)
+    # Std of Gaussian exploration noise
+    arg_conf.add_argument("--expl_noise")
+    # Batch size for both actor and critic
+    arg_conf.add_argument("--batch_size", type=int)
+    arg_conf.add_argument("--discount")                 # Discount factor
+    # Target network update rate
+    arg_conf.add_argument("--tau")
+    # Noise added to target policy during critic update
+    arg_conf.add_argument("--policy_noise")
+    # Range to clip target policy noise
+    arg_conf.add_argument("--noise_clip")
+    # Frequency of delayed policy updates
+    arg_conf.add_argument("--policy_freq", type=int)
+    # Save model and optimizer parameters
+    arg_conf.add_argument("--save_model", action="store_true")
+    # Model load file name, "" doesn't load, "default" uses file_name
+    arg_conf.add_argument("--load_model", default="")
+
+    # Leave unchanged
+    parser = add_launcher_base_args(parser)
+    parser.set_defaults(**get_default_params(experiment))
+    args = parser.parse_args()
+    return vars(args)
 
 
-def create_policy_eval_video(policy, filename, num_episodes=5, fps=60):
-	filename = filename + ".mp4"
-	with imageio.get_writer(filename, fps=fps) as video:
-		for _ in range(num_episodes):
-			env.reset()
-			state = get_screen()
-			stacked_states = collections.deque(STACK_SIZE*[state],maxlen=STACK_SIZE)
-			done = False
-			video.append_data(env.render())
-			#video.append_data(state[0,0].numpy())
-			while not done:
-				stacked_states_t =  torch.cat(tuple(stacked_states),dim=1)
-				# Select and perform an action
-				action = policy_net(stacked_states_t).max(1)[1].view(1, 1)
-				_, reward, done, _ = env.step(action.item())
-				# Observe new state
-				next_state = get_screen()
-				#print(next_state[0,0].numpy().type)
-				stacked_states.append(next_state)
-				video.append_data(env.render())
-				#video.append_data(next_state[0,0].numpy())
-	return embed_mp4(filename)
-
-
-# helper function to extract the screen from the environment
-def get_screen(preprocess):
-	global stacked_screens
-	screen = env._get_observation().transpose((2, 0, 1))
-	
-	screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
-	screen = torch.from_numpy(screen)
-	# Resize, and add a batch dimension (BCHW)
-	return preprocess(screen).unsqueeze(0).to(device)
-
+# ----------------
 # main program
+# ----------------
+
 if __name__ == '__main__':
-	
-	# load and reset environment
-	env = KukaDiverseObjectEnv(renders=False, isDiscrete=True, removeHeightHack=False, maxSteps=20)
-	env.cid = p.connect(p.DIRECT)
-	env.reset()
-	
-	# define image preprocessing
-	preprocess = T.Compose([T.ToPILImage(),
-		T.Grayscale(num_output_channels=1),
-		T.Resize(40, interpolation=Image.CUBIC),
-		T.ToTensor()])
-	
-	# setup save directory
-	save_dir = Path('checkpoints') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
-	save_dir.mkdir(parents=True)
-	
-	checkpoint = None if not(args.evaluate) else None
-	
-	# choose algorithm/agent
-	if args.algorithm == 'dqn':
-		action_space_n = env.action_space.n	
-		agent = AgentQ(state_dim=10, action_dim=action_space_n, save_dir=save_dir, checkpoint=checkpoint)
-	elif args.algorithm == 'ppo':
-		raise NotImplementedError('{} is not yet implemented'.format(args.algorithm))
-	else:
-		raise NotImplementedError('{} is not yet implemented'.format(args.algorithm))
-	
-	
-	logger = MetricLogger(save_dir)
-	tblogger = TBMetricLogger(save_dir)
-	episodes = 10000000
-	
-	
-	if __name__ == '__main__':
-		pass
-	
-	for e in range(episodes):
-	
-		state = env.reset()
-	
-		while True:
-	
-			# 3. Render environment (the visual) [WIP]
-			# env.render()
-			
-			# 4. Run agent on the state
-			action = agent.act(state)
-			#action = env.action_space.sample()
-	
-			# 5. Agent performs action
-			next_state, reward, done, info = env.step(action)
-			
-			if args.verbose:
-				print("Action Taken  ",action)
-				print("Observation   ",next_state)
-				print("Reward Gained ",reward)
-				print("Info          ",info,end='\n\n')
-			
-			# 6. Remember
-			agent.add_to_memory(state, next_state, action, reward, done)
-	
-			# 7. Learn
-			q, loss = agent.learn()
-	
-			# 8. Logging
-			logger.log_step(reward, loss, q)
-			tblogger.log_step(reward, loss, q, success=env.landed_ticks)
-	
-			# 9. Update state
-			state = next_state
-	
-			# 10. Check if end of game
-			if done:
-				break
-	
-		logger.log_episode()
-		tblogger.log_episode()
-		
-		if e % 20 == 0:
-			logger.record(
-				episode=e,
-				epsilon=agent.exploration_rate,
-				step=agent.curr_step
-			)
-	
-	tblogger.writer.close()
-	
-	# final policy evaluation
-	create_policy_eval_video(policy_net, "final_performance", num_episodes=10, fps=10)
+    # Leave unchanged
+    args = parse_args()
+    run_experiment(experiment, args)
+
+
+# _____________________________________________________________________________
+
+# Stick to 80 characters per line
+# Use PEP8 Style
+# Comment your code
+
+# -----------------
+# top-level comment
+# -----------------
+
+# medium level comment
+# -----
+
+# low level comment
