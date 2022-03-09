8e49712dda04f96940d9333a74f8121e83e6d98a
Markus Ernst <markusrolandernst@t-online.de>
2022-03-09 16:05:59 +0000

    minor changes to replay buffer

--- algo/td3.py
+++ algo/td3.py
@@ -129,7 +129,7 @@ class TD3(object):
         self.total_it += 1
 
         # Sample replay buffer
-        state, action, next_state, reward, not_done = replay_buffer.sample(
+        _, _, state, action, next_state, reward, not_done = replay_buffer.sample(
             batch_size)
 
         with torch.no_grad():
--- util/replay_buffer.py
+++ util/replay_buffer.py
@@ -11,15 +11,21 @@ import numpy as np
 import torch
 
 
+
 # custom classes
 # -----
 
 class ReplayBuffer(object):
-    def __init__(self, state_dim, action_dim, max_size=int(1e6)):
+    def __init__(self, state_dim, action_dim, sequence_dim, offpolicy, max_size=int(1e6)):
+                
         self.max_size = max_size
         self.ptr = 0
         self.size = 0
-
+        
+        sequence_dim = sequence_dim if offpolicy else 1
+        self.state_seq = np.zeros((max_size, sequence_dim, state_dim))  
+        self.action_seq = np.zeros((max_size, sequence_dim, action_dim))
+        
         self.state = np.zeros((max_size, state_dim))
         self.action = np.zeros((max_size, action_dim))
         self.next_state = np.zeros((max_size, state_dim))
@@ -29,7 +35,9 @@ class ReplayBuffer(object):
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
 
-    def add(self, state, action, next_state, reward, done):
+    def add(self, state_seq, action_seq, state, action, next_state, reward, done):
+        self.state_seq[self.ptr] = state_seq
+        self.action_seq[self.ptr] = action_seq
         self.state[self.ptr] = state
         self.action[self.ptr] = action
         self.next_state[self.ptr] = next_state
@@ -40,16 +48,34 @@ class ReplayBuffer(object):
         self.size = min(self.size + 1, self.max_size)
 
     def sample(self, batch_size):
-        ind = np.random.randint(0, self.size, size=batch_size)
+        ind = _get_indices(size=batch_size)
 
         return (
+            torch.FloatTensor(self.state_seq[ind]).to(self.device),
+            torch.FloatTensor(self.action_seq[ind]).to(self.device),
             torch.FloatTensor(self.state[ind]).to(self.device),
             torch.FloatTensor(self.action[ind]).to(self.device),
             torch.FloatTensor(self.next_state[ind]).to(self.device),
             torch.FloatTensor(self.reward[ind]).to(self.device),
             torch.FloatTensor(self.not_done[ind]).to(self.device)
         )
+    
+    def _get_indices(self, size):
+        return np.random.randint(0, self.size, size=size)
 
+
+class PERBuffer(ReplayBuffer):
+    """
+    Prioritized Experience Replay
+    """
+    def __init__(self, ):
+        raise NotImplementedError("Prioritized Experience Replay is not yet implemented")
+    
+    def add():
+        pass
+    
+    def sample():
+        pass
 
 # ----------------
 # main program
