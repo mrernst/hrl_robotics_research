4db6a68bc028f11636c41676c34cf2bda1a84162
Markus Ernst <markusrolandernst@t-online.de>
2022-03-11 09:47:50 +0000

    removed old environment files

--- env/mujoco/point_maze_env.py
+++
@@ -1,6 +0,0 @@
-from .maze_env import MazeEnv
-from .point import PointEnv
-
-
-class PointMazeEnv(MazeEnv):
-    MODEL_CLASS = PointEnv
--- env/mujoco/swimmer_maze_env.py
+++
@@ -1,6 +0,0 @@
-from .maze_env import MazeEnv
-from .swimmer import SwimmerEnv
-
-
-class SwimmerMazeEnv(MazeEnv):
-    MODEL_CLASS = SwimmerEnv
