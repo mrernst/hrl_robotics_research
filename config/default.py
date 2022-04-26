d7e18e5b0d7d59d8e6d138caa5f8ddb051cbc79d
Markus Ernst <markusrolandernst@t-online.de>
2022-04-26 11:55:32 +0000

    update default Maze to be larger

--- config/default.py
+++ config/default.py
@@ -25,7 +25,7 @@ def get_config():
 		{
 			'main': {
 				# OpenAI gym environment name
-				'env_name': "AntMaze1-v1",
+				'env_name': "AntMaze1-v0",
 				# General Training parameter
 				'train': True,
 				# General Evaluation parameter
