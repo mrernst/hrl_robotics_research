efd7a35363c77ecdc64c23c04271b018883b7dfc
Markus Ernst <markusrolandernst@t-online.de>
2022-05-05 07:11:55 +0000

    temporary changes only compatible w goalplane env

--- agent/base.py
+++ agent/base.py
@@ -63,39 +63,43 @@ class Agent(object):
 		success = 0
 		rewards = []
 		env.evaluate = True
-		for e in range(eval_episodes):
-			obs = env.reset()
-			fg = obs['desired_goal']
-			fg_dim = fg.shape[0]
-			s = obs['observation']
-			done = False
-			reward_episode_sum = 0
-			step = 0
-			
-			self.set_final_goal(fg)
-	
-			while not done:
-				if render:
-					env.render()
-				if sleep>0:
-					time.sleep(sleep)
-	
-				a, r, n_s, done = self.step(s, env, step)
-				reward_episode_sum += r
+		with torch.no_grad():
+			for e in range(eval_episodes):
+				obs = env.reset()
+				fg = obs['desired_goal']
+				fg_dim = fg.shape[0]
+				s = obs['observation']
+				done = False
+				reward_episode_sum = 0
+				step = 0
 				
-				s = n_s
-				step += 1
-				self.end_step()
-				if save_video:
-					video.append_data(env.render(mode='rgb_array'))
-			else:
-				error = np.sqrt(np.sum(np.square(fg-s[:fg_dim])))
-				print(" " * 80 + "\r" +
-				'[Eval] Goal, Curr: (%02.2f, %02.2f, %02.2f, %02.2f)     Error:%.2f'%(fg[0], fg[1], s[0], s[1], error), end='\r')
-				rewards.append(reward_episode_sum)
-				success += 1 if error <=5 else 0
-				# this is not suited for every environment, distance should be adapted
-				self.end_episode(e)
+				self.set_final_goal(fg)
+		
+				while not done:
+					if render:
+						env.render(subgoal=self.sg+s[:self.sg.shape[0]]) #if possible render subgoal
+						#env.render() #if possible render subgoal
+						#print(self.sg, self.sg+s[:2])
+					if sleep>0:
+						time.sleep(sleep)
+		
+					a, r, n_s, done = self.step(s, env, step)
+					reward_episode_sum += r
+					
+					s = n_s
+					step += 1
+					self.end_step()
+					if save_video:
+						video.append_data(env.render(subgoal=self.sg+s[:self.sg.shape[0]], mode='rgb_array'))
+						#video.append_data(env.render(mode='rgb_array'))
+				else:
+					error = np.sqrt(np.sum(np.square(fg-s[:fg_dim])))
+					print(" " * 80 + "\r" +
+					'[Eval] Goal, Curr: (%02.2f, %02.2f, %02.2f, %02.2f)     Error:%.2f'%(fg[0], fg[1], s[0], s[1], error), end='\r')
+					rewards.append(reward_episode_sum)
+					success += 1 if error <=5 else 0
+					# this is not suited for every environment, distance should be adapted
+					self.end_episode(e)
 	
 		env.evaluate = False
 		return np.array(rewards), success/eval_episodes
