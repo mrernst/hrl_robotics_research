41fa1d6e436c6fd86f22b4e26041a4d4d295da32
Markus Ernst <markusrolandernst@t-online.de>
2022-06-05 15:03:00 +0000

    off-policy correction mods

--- algo/td3.py
+++ algo/td3.py
@@ -348,7 +348,8 @@ class HighLevelController(TD3Controller):
         spec_range = low_con.max_action.cpu().data.numpy()
         # Sample from normal distribution
         loc = (next_states - states)[:, np.newaxis, :goal_dim]
-        scale = 0.5 * self.max_action[None, None, :]
+        # scale = 0.5 * self.max_action[None, None, :]
+        scale = 0.25 * self.max_action #c.f. HIRO Paper
         original_goal = np.array(actions[:, np.newaxis, :])
         random_goals = np.random.normal(loc=loc, scale=scale, size=(batch_size, candidate_goals, original_goal.shape[-1]))
         candidates = np.concatenate([original_goal, loc, random_goals], axis=1)
@@ -382,84 +383,15 @@ class HighLevelController(TD3Controller):
         normalized_error = - np.square(difference) / np.square(spec_range)
         #print(normalized_error.shape)
         fitness = np.sum(normalized_error, axis=(2, 3))
+        # - 0.5 * tf.reduce_sum(tf.square(tf.norm(diffn, axis=2)), axis=1) ??
         best_actions = np.argmax(fitness, axis=-1)
         
         return candidates[np.arange(batch_size), best_actions]
 
-#     def off_policy_corrections(
-#         self,
-#         low_con,
-#         batch_size,
-#         sgoals,
-#         states,
-#         actions,
-#         candidate_goals=8
-#     ):
-# 
-#         first_s = [s[0] for s in states]  # First x
-#         last_s = [s[-1] for s in states]  # Last x
-# 
-#         # Shape: (batch_size, 1, subgoal_dim)
-#         # diff = 1
-#         diff_goal = (np.array(last_s) -
-#                      np.array(first_s))[:, np.newaxis, :self.action_dim]
-# 
-#         # Shape: (batch_size, 1, subgoal_dim)
-#         # original = 1
-#         # random = candidate_goals
-#         original_goal = np.array(sgoals)[:, np.newaxis, :]
-#         random_goals = np.random.normal(loc=diff_goal, scale=.5*self.max_action[None, None, :],
-#                                         size=(batch_size, candidate_goals, original_goal.shape[-1]))
-#         random_goals = random_goals.clip(-self.max_action, self.max_action)
-#         # Shape: (batch_size, 10, subgoal_dim)
-#         candidates = np.concatenate(
-#             [original_goal, diff_goal, random_goals], axis=1)
-#         #states = np.array(states)[:, :-1, :]
-#         actions = np.array(actions)
-#         seq_len = len(states[0])
-#         #print(candidates.dtype)# -> float64
-# 
-#         # For ease
-#         new_batch_sz = seq_len * batch_size
-#         action_dim = actions[0][0].shape
-#         obs_dim = states[0][0].shape
-#         ncands = candidates.shape[1]
-# 
-#         true_actions = actions.reshape((new_batch_sz,) + action_dim)
-#         observations = states.reshape((new_batch_sz,) + obs_dim)
-#         goal_shape = (new_batch_sz, self.action_dim)
-#         # observations = get_obs_tensor(observations, sg_corrections=True)
-# 
-#         # batched_candidates = np.tile(candidates, [seq_len, 1, 1])
-#         # batched_candidates = batched_candidates.transpose(1, 0, 2)
-# 
-#         policy_actions = np.zeros((ncands, new_batch_sz) + action_dim)
-# 
-#         for c in range(ncands):
-#             subgoal = candidates[:, c]
-#             candidate = (subgoal + states[:, 0, :self.action_dim]
-#                          )[:, None] - states[:, :, :self.action_dim]
-#             candidate = candidate.reshape(*goal_shape)
-#             policy_actions[c] = low_con.policy(observations, candidate).reshape(policy_actions.shape[1:]) #TODO: this reshaping maybe does not have to be done manually here # also the low_con_policy should not be trained here?
-# 
-#         difference = (policy_actions - true_actions)
-#         difference = np.where(difference != -np.inf, difference, 0)
-#         difference = difference.reshape(
-#             (ncands, batch_size, seq_len) + action_dim).transpose(1, 0, 2, 3)
-# 
-#         logprob = -0.5*np.sum(np.linalg.norm(difference, axis=-1)**2, axis=-1)
-#         max_indices = np.argmax(logprob, axis=-1)
-#         return candidates[np.arange(batch_size), max_indices]
 
     def train(self, replay_buffer, low_con):
         states, goals, actions, n_states, rewards, not_done, states_arr, actions_arr = replay_buffer.sample()
-
-        # actions = self.off_policy_corrections(
-        #     low_con,
-        #     replay_buffer.batch_size,
-        #     actions.cpu().data.numpy(),
-        #     states_arr.cpu().data.numpy(),
-        #     actions_arr.cpu().data.numpy())
+        
         
         actions = self._off_policy_corrections(
             low_con,
@@ -475,6 +407,92 @@ class HighLevelController(TD3Controller):
 
     pass
 
+# 
+# 
+# def off_policy_correction_tf(subgoal_ranges, target_dim, pi, goal_b, state_b, next_state_b, no_candidates, c_step, state_seq,
+#                           action_seq, zero_obs):
+#     # TODO Update docstring to real dimensions
+#     '''Computes the off-policy correction for the meta-agent.
+#     c = candidate_nbr; t = time; i = vector coordinate (e.g. action 0 of 8 dims) 
+#     Dim(candidates) = [b_size, g_dim, no_candidates+2 ]
+#     Dim(state_b)     = [b_size, state_dim]
+#     Dim(action_seq)    = []
+#     Dim(prop_goals) = [c_step, b_size, g_dim, no_candidates]'''
+#     b_size = state_b.shape[0] # Batch Size
+#     g_dim = goal_b[0].shape[0] # Subgoal dimension
+#     action_dim = action_seq.shape[-1] # action dimension
+#     # States contains state+target. Need to take out only the state.
+#     state_seq = state_seq[:,:, :-target_dim]
+#     state_b = state_b[:, :-target_dim]
+#     next_state_b = next_state_b[:, :-target_dim]
+#     # Get the sampled candidates
+#     candidates =  _get_goal_candidates(b_size, g_dim, no_candidates, subgoal_ranges, state_b, next_state_b, goal_b)
+#     # Take all the possible candidates and propagate them through time using h = st + gt - st+1 cf. HIRO Paper
+#     prop_goals = _multi_goal_transition(state_seq, candidates, c_step)
+#     # Zero out xy for sub agent, AFTER goals have been calculated from it.
+#     state_seq = tf.reshape(state_seq, [b_size * c_step, state_seq.shape[-1]])
+#     if zero_obs:
+#         state_seq *= tf.concat([tf.zeros([state_seq.shape[0], zero_obs]), tf.ones([state_seq.shape[0], state_seq.shape[1] - zero_obs])], axis=1)
+#     best_c = _get_best_c(b_size, c_step, action_dim, g_dim, no_candidates, action_seq, state_seq, prop_goals, pi) 
+#     return _get_corrected_goal(b_size, candidates, best_c) 
+#     
+# def _multi_goal_transition(state_seq, candidates, c_step):
+#     # Realize that the multi timestep goal transition can be vectorized to a single calculation.
+#     b_size = candidates.shape[0]
+#     g_dim = candidates.shape[1]
+#     c_step = state_seq.shape[1] 
+#     no_candidates = candidates.shape[2]
+#     # In state_seq, equal batches are separated by c timesteps if we query a subogoal every c_steps 
+#     prop_goals = tf.broadcast_to(tf.expand_dims(candidates, axis=1), [b_size, c_step, g_dim, no_candidates])
+#     tmp = tf.broadcast_to(tf.expand_dims(state_seq[:,0,:g_dim], axis=1), [b_size, c_step, g_dim]) - state_seq[:, :, :g_dim] 
+#     prop_goals += tf.broadcast_to(tf.expand_dims(tmp, axis=3), [b_size, c_step, g_dim, no_candidates]) 
+#     return prop_goals 
+#     
+# def _get_goal_candidates(b_size, g_dim, no_candidates, subgoal_ranges, state_b, next_state_b, goal_b):
+#     # Original Goal
+#     orig_goal = tf.expand_dims(goal_b[:, :g_dim], axis=2)
+#     # Goal between the states s_t+1 - s_t
+#     diff_goal = tf.expand_dims(next_state_b[:, :g_dim] - state_b[:, :g_dim], axis=2)
+#     goal_mean = tf.broadcast_to(diff_goal, [b_size, g_dim, no_candidates])
+#     # Broadcast the subgoal_ranges to [b_size, g_dim, no_candidates] for use as clipping and as std
+#     clip_tensor = tf.expand_dims(tf.broadcast_to(subgoal_ranges, [b_size, subgoal_ranges.shape[0]]), axis=2)
+#     clip_tensor = tf.broadcast_to(clip_tensor, [b_size, subgoal_ranges.shape[0], no_candidates+2])
+#     goal_std = 0.25 * clip_tensor # cf. HIRO Paper
+#     candidates = tf.random.normal([b_size, g_dim, no_candidates], goal_mean, goal_std[:, :, :no_candidates])
+#     candidates = tf.concat([orig_goal, diff_goal, candidates], axis=2)
+#     candidates = tf.clip_by_value(candidates, -clip_tensor, clip_tensor)
+#     return candidates
+# 
+# def _get_corrected_goal(b_size, candidates, best_c): 
+#     corrected_goals = tf.TensorArray(dtype=tf.float32, size=b_size)
+#     for b in tf.range(b_size):
+#         corrected_goals = corrected_goals.write(b, candidates[b, :, best_c[b]])
+#     return corrected_goals.stack() 
+#     
+# def _get_best_c(b_size, c_step, action_dim, g_dim, no_candidates, action_seq, state_seq, prop_goals, pi):
+#     # Compute the logpropabilities for different subgoal candidates
+#     max_logprob = tf.constant(-1e9, shape=[b_size,])
+#     best_c = tf.zeros([b_size,], dtype=tf.int32) # In Graph mode, elements need to be defined BEFORE the loop
+#     action_seq = tf.reshape(action_seq, [b_size * c_step, action_dim])
+#     for c in tf.range(no_candidates):
+#         goals = prop_goals[:, :, :, c]  # Pick one candidate 
+#         # Change dimension of goals so that it can be computed in one batch in the network [b_size * c_step, g_dim]
+#         goals = tf.reshape(goals, [b_size * c_step, g_dim])
+#         state_action = tf.concat([state_seq, goals], axis=1)
+#         pred_action =  pi(state_action)
+#         diff = action_seq - pred_action
+#         # Have padded the sequences where len(seq)<c_step to the max(c_step) with np.inf
+#         # This results in nan in the computed action differences. These are now set to 0
+#         # as 0 does not influence the sum over all actions that happens later.
+#         diff = tf.where(tf.math.is_nan(diff), 0., diff)
+#         # This reshape only works when rehaping consecutive dimension (e.g. [2560,8] to [256,10,8] or vice versa. 
+#         # Else elements will be thrown around.
+#         diffn = tf.reshape(diff, [b_size, c_step, action_dim])
+#         # cf. HIRO Paper
+#         logprob = - 0.5 * tf.reduce_sum(tf.square(tf.norm(diffn, axis=2)), axis=1)
+#         best_c = tf.where(logprob > max_logprob, c, best_c)
+#         max_logprob = tf.where(logprob > max_logprob, logprob, max_logprob)
+#     return best_c
 
 class LowLevelController(TD3Controller):
 
