19d245e9d720a20a520cf17aff7ea4d023956ca5
Markus Ernst <markusrolandernst@t-online.de>
2022-06-08 09:11:09 +0000

    new exp

--- ignition.py
+++ ignition.py
@@ -27,7 +27,7 @@ def main(_argv):
     JOBLIB_PARALLEL_JOBS = 2  # or os.cpu_count() to use all cores
     N_SEEDS = 1
     
-    launcher = Launcher(exp_name='xx1_lrates',
+    launcher = Launcher(exp_name='xx1_ant3dsub',
                         python_file='main',
                         project_name='luna',
                         base_dir='./save/',
@@ -50,41 +50,41 @@ def main(_argv):
                         )
     
     
-    actor_learning_rates = [0.0001, 0.001, 0.01]
-    critic_learning_rates = [0.0001, 0.001, 0.01]
-    # #b_c_list = [11, 12]
-    # #boolean_list = [True, False]
-    # 
-    # #launcher.add_default_params(default='b')
-    # 
-    for ac_lr, cr_lr in product(actor_learning_rates, critic_learning_rates):
-        # get the default parameters agent and main
-        # add experiments directly to the config
-        # b/c joblib passes arguments to experiment
-        if LOCAL:
-            FLAGS.config.agent.sub.actor_lr = ac_lr
-            FLAGS.config.agent.sub.critic_lr = cr_lr
-            launcher.add_experiment(**FLAGS.config)
-        else:
-            launcher.add_experiment(**{
-                'config.main.env_name': 'PointMaze1-v0',
-                'config.main.max_timesteps': 5e6,
-                'config.agent.sub.actor_lr': ac_lr,
-                'config.agent.sub.critic_lr': cr_lr,
-                'config.agent.agent_type': 'hiro',
-                'config.agent.subgoal_dim': 3,
-            })
-    
-    # agent_type = ['flat', 'hiro']
-    # for at in agent_type:
+    # actor_learning_rates = [0.0001, 0.001, 0.01]
+    # critic_learning_rates = [0.0001, 0.001, 0.01]
+    # # #b_c_list = [11, 12]
+    # # #boolean_list = [True, False]
+    # # 
+    # # #launcher.add_default_params(default='b')
+    # # 
+    # for ac_lr, cr_lr in product(actor_learning_rates, critic_learning_rates):
+    #     # get the default parameters agent and main
+    #     # add experiments directly to the config
+    #     # b/c joblib passes arguments to experiment
     #     if LOCAL:
-    #         pass
+    #         FLAGS.config.agent.sub.actor_lr = ac_lr
+    #         FLAGS.config.agent.sub.critic_lr = cr_lr
+    #         launcher.add_experiment(**FLAGS.config)
     #     else:
     #         launcher.add_experiment(**{
-    #             'config.agent.agent_type': at,
     #             'config.main.env_name': 'PointMaze1-v0',
+    #             'config.main.max_timesteps': 5e6,
+    #             'config.agent.sub.actor_lr': ac_lr,
+    #             'config.agent.sub.critic_lr': cr_lr,
+    #             'config.agent.agent_type': 'hiro',
     #             'config.agent.subgoal_dim': 3,
     #         })
+    # 
+    agent_type = ['flat', 'hiro']
+    for at in agent_type:
+        if LOCAL:
+            pass
+        else:
+            launcher.add_experiment(**{
+                'config.agent.agent_type': at,
+                #'config.main.env_name': 'PointMaze1-v0',
+                'config.agent.subgoal_dim': 3,
+            })
     
     launcher.run(LOCAL, TEST)
     
