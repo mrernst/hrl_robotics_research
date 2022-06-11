ddebf52aa150d246c80b01a10725c137f7fe016d
Markus Ernst <markusrolandernst@t-online.de>
2022-06-11 06:42:03 +0000

    big update, now functional tool

--- util/asciitensorboard.py
+++ util/asciitensorboard.py
@@ -15,7 +15,17 @@ import plotext as plt
 # -----
 from afterburner import read_multiple_runs, convert_em_to_df
 
-
+def let_user_pick(options):
+    print("Please choose:")
+    for idx, element in enumerate(options):
+        print("{}) {}".format(idx+1,element))
+    i = input("Enter number: ")
+    try:
+        if 0 < int(i) <= len(options):
+            return int(i) - 1
+    except:
+        pass
+    return None
 
 
 
@@ -24,43 +34,126 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("--logdir", "-d",  default='./save/', type=str)
-    parser.add_argument("--stream", "-s",  default=False, type=bool)
     parser.add_argument("--rate", "-r", default=30, type=int)
-
-    args = parser.parse_args()
     
-    print(args)
+    parser.add_argument('--present_options', '-o',
+                        dest='present_options',
+                        action='store_true')
+    parser.add_argument('--no-present_options', '-no',
+                        dest='present_options',
+                        action='store_false')
+    parser.set_defaults(present_options=True)
+    
+    parser.add_argument('--combine_views', '-c',
+                        dest='combine_views',
+                        action='store_true')
+    parser.add_argument('--no-combine_views', '-nc',
+                        dest='combine_views',
+                        action='store_false')
+    parser.set_defaults(combine_views=False)
+    
+    parser.add_argument('--stream', '-s',
+                        dest='stream',
+                        action='store_true')
+    parser.add_argument('--no-stream', '-ns',
+                        dest='stream',
+                        action='store_false')
+    parser.set_defaults(stream=False)
+
+
     
     # read logdir argument, read the stream argument, read the refreshrate argument
+    args = parser.parse_args()    
     
     # read the directory
     em = read_multiple_runs(args.logdir)
     df = convert_em_to_df(em)
-    # display options of data to display, let user choose with a number
     
-    # wait on user input
+    # display options of data to display, let user choose with a number
+    user_picked_run = None
+    if args.present_options or args.stream:
+        # wait on user input
+        user_picked_run = let_user_pick(list(df.keys())+['combine views'])
     #print(df.keys())
     #print(df['training'].keys())
     # display plot, go backplaying of options, or stream continuously
     
-    for k1 in df.keys():
-        for k2 in df[k1].keys():
-            if 'loss' in k2 or 'reward' in k2 or 'success' in k2:
-                plt.plot(df[k1][k2].values)
-                plt.title(f"{k2}")
+
+    
+    if args.combine_views or user_picked_run==len(df.keys()):
+        reverse_dict_of_keys = {}
+        for k1 in df.keys():
+            for k2 in df[k1].keys():
+                if k2 in reverse_dict_of_keys.keys():
+                    reverse_dict_of_keys[k2].append(k1)
+                else:
+                    reverse_dict_of_keys[k2] = []
+                    reverse_dict_of_keys[k2].append(k1)
+        
+        if args.stream:
+            # show options and ask user
+            user_picked_metric = let_user_pick([k for k in reverse_dict_of_keys.keys() if len(reverse_dict_of_keys[k])>1])
+            # enter streaming loop
+            k2 = list(df[k1].keys())[user_picked_metric]
+            plt.title(f"Streaming: {k2}")
+            while True:
+                plt.clt()
+                plt.cld()
+                for k1 in reverse_dict_of_keys[k2]:
+                    plt.plot(df[k1][k2].values, label=k1)
                 plt.show()
-                plt.clf()
-    # plt.plot(df['agent']['agent/sub/actor/weights/min'].values, df['training']['step'].values)
-    # plt.title('agent/sub/actor/weights/min')
-    #plt.plot(df['agent']['agent/meta/actor/weights/min'].values)
+                
+                plt.sleep(args.rate)
+                em.Reload()
+                df = convert_em_to_df(em)
 
-    #plt.show()
+        else:
+            #plot all
+            for k2 in reverse_dict_of_keys.keys():
+                if len(reverse_dict_of_keys[k2]) > 1 and ('loss' in k2 or 'reward' in k2 or 'success' in k2):
+                    for k1 in reverse_dict_of_keys[k2]:
+                        plt.plot(df[k1][k2].values, label=k1)
+                    plt.title(f"{k2}")
+                    plt.show()
+                    plt.clf()
+    else:
+        if isinstance(user_picked_run, int):
+            k1 = list(df.keys())[user_picked_run]
+            if args.stream:
+                # show options and ask user
+                user_picked_metric = let_user_pick(list(df[k1].keys()))
+                # enter streaming loop
+                k2 = list(df[k1].keys())[user_picked_metric]
+                plt.title(f"Streaming: {k2}")
+                while True:
+                    plt.clt()
+                    plt.cld()
+                    plt.plot(df[k1][k2].values)
+                    plt.show()
+                    
+                    plt.sleep(args.rate)
+                    em.Reload()
+                    df = convert_em_to_df(em)
+            else:
+                #plot all
+                for k2 in df[k1].keys():            
+                    if 'loss' in k2 or 'reward' in k2 or 'success' in k2:
+                        plt.plot(df[k1][k2].values)
+                        plt.title(f"{k2}")
+                        plt.show()
+                        plt.clf()
+        else:
+            # plot everything
+            for k1 in df.keys():
+                for k2 in df[k1].keys():            
+                    if 'loss' in k2 or 'reward' in k2 or 'success' in k2:
+                        plt.plot(df[k1][k2].values)
+                        plt.title(f"{k2}")
+                        plt.show()
+                        plt.clf()
 
 
-# _____________________________________________________________________________
 
-
-# import the tensorboard reader functions written for saturn or titan?
 # _____________________________________________________________________________
 
 # Stick to 80 characters per line
