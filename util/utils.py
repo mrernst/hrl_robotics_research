e16e8e12e04d71654b388e1e9c32db04efb23166
Markus Ernst <markusrolandernst@t-online.de>
2022-08-26 09:41:02 +0000

    added AverageMeter class

--- util/utils.py
+++ util/utils.py
@@ -84,6 +84,22 @@ def print_tensor_info(tensor, name=None, writer=None):
         
     pass
 
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+    
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
 
 
 def random_sample(size=None, dtype=np.float64):
